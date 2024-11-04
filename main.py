d9e35ad4c45b6745a6397b082113a8d4cde1c9f2
miguel <jmiguel.hdez@gmail.com>
2024-11-04 09:01:55 -0600

    clean up

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(BLACK_COLOR)
+        screen.fill(pygame.Color("black"))
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
